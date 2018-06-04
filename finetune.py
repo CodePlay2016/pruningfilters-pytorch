b3431b6bac377fab01c343a7afd19c58f596f4a8
CodePlay2016 <13269308268@163.com>
2018-06-04 18:50:11 +0800

    convert torch variable to numpy

--- finetune.py
+++ finetune.py
@@ -83,8 +83,7 @@ class FilterPrunner:
         activation_index = len(self.activations) - \
             self.grad_index - 1  # calculate from the bottom
         activation = self.activations[activation_index]
-        print str(type(activation.data))
-        values = np.multiply(activation.data,grad.data)
+        values = np.multiply(activation.data.numpy(),grad.data.numpy())
         values = np.sum(values, axis=0).sum(axis=1).sum(axis=1)
         # values = np.sum(values, axis=1)
         # values = np.sum(values, axis=1)
